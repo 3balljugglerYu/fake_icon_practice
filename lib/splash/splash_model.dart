85b58397dc504050750eb086be13fdf05e255eac
3balljuggler <3balljuggling@gmail.com>
2022-12-27 18:02:58 +0900

    splashPageで表示されるロゴのアニメーションを改善

--- lib/splash/splash_model.dart
+++ lib/splash/splash_model.dart
@@ -5,136 +5,176 @@ import 'package:fake_icon_practice/utils.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
-class SplashModel extends ChangeNotifier {
-  Position logoPosition = Position(x: 0, y: 0);
-  Size displaySize = const Size(0, 0);
-  Size logoSize = const Size(200, 200);
-  bool isSetUpFinish = true;
-  bool isLogoVisibility = true;
-
-  void measureDisplaySize(Size size) async {
-    displaySize = size;
-  }
-
-  Future<void> setUp() async {
-    logoPosition.x = (displaySize.width - logoSize.width) / 2;
-    logoPosition.y = (displaySize.height - logoSize.height) / 2;
-    isSetUpFinish = false;
-    await Future.delayed(const Duration(milliseconds: 1));
-    notifyListeners();
-  }
-
-  void changeLogoPosition(DraggableDetails details) {
-    logoPosition.x = details.offset.dx;
-    logoPosition.y = details.offset.dy;
-    logoVisibility();
-    notifyListeners();
-  }
-
-  void logoVisibility() {
-    if(logoPosition.x > displaySize.width - (logoSize.width / 2) || logoPosition.x < -(logoSize.width / 2)){
-      isLogoVisibility = false;
-    }
-    if(logoPosition.y < -(logoSize.height / 2) || logoPosition.y > displaySize.height - (logoSize.height /2)){
-      isLogoVisibility = false;
-    }
-  }
-
-  Future<void> comeBackLogo() async {
-    setUp();
-    await Future.delayed(Duration(seconds: Utils.comeBackSecond));
-    isLogoVisibility = true;
-    notifyListeners();
-  }
-
-  Future<void> transitionScreen(BuildContext context) async {
-    await Future.delayed(Duration(seconds: Utils.transitionSecond));
-    Navigator.push(context, MaterialPageRoute(builder: (context) => FakePage()));
-  }
-
-}
-
 // class SplashModel extends ChangeNotifier {
 //   Position logoPosition = Position(x: 0, y: 0);
-//
-//   //isSetUpはアプリ起動の再現が行われたかの判断
-//   bool isSetUpFinish = false;
-//   bool isAnimationFinish = false;
-//   bool isLogoVisibility = true;
-//   bool isComeBackLogo = false;
-//
 //   Size displaySize = const Size(0, 0);
 //   Size logoSize = const Size(200, 200);
-//
-//   Future<void> setUp() async {
-//     logoPosition.x = (displaySize.width - logoSize.width) / 2;
-//     logoPosition.y = (displaySize.height - logoSize.height) / 2;
-//
-//     //画面が立ち上がってから、アニメーションが発生するまでの長さ
-//     await Future.delayed(const Duration(milliseconds: 100));
-//     isSetUpFinish = true;
-//
-//     notifyListeners();
-//
-//     //スプラッシュ画面が表示される秒数
-//     await Future.delayed(const Duration(seconds: 1));
-//   }
-//
-//   Future<void> changeLogoPosition(
-//       DraggableDetails details, BuildContext context) async {
-//     logoPosition.x = details.offset.dx;
-//     logoPosition.y = details.offset.dy;
-//     await logoVisibility(context);
-//     notifyListeners();
-//   }
-//
-//   Future<void> logoVisibility(BuildContext context) async {
-//     if (logoPosition.x < -(logoSize.width / 3) ||
-//         logoPosition.x + logoSize.width - displaySize.width >
-//             logoSize.width / 3) {
-//       isLogoVisibility = false;
-//     }
-//     if (logoPosition.y < -(logoSize.height / 3) ||
-//         logoPosition.y + logoSize.height - displaySize.height >
-//             logoSize.height / 3) {
-//       isLogoVisibility = false;
-//     }
-//   }
-//
-//   Future<void> comeBackLogo(BuildContext context) async {
-//     setUp();
-//     await Future.delayed(
-//         Duration(seconds: Utils.comeBackSecond - Utils.notifySecond))
-//         .then((value) {
-//       HapticFeedback.heavyImpact();
-//     });
-//     await Future.delayed(Duration(seconds: Utils.notifySecond));
-//     isComeBackLogo = true;
-//     notifyListeners();
-//   }
-//
-//   void transitionScreen(BuildContext context) {
-//     Navigator.of(context).push(
-//       PageRouteBuilder(
-//         pageBuilder: (context, animation, secondaryAnimation) {
-//           return FakePage();
-//         },
-//         transitionsBuilder: (context, animation, secondaryAnimation, child) {
-//           const double begin = 0.0;
-//           const double end = 1.0;
-//           final Animatable<double> tween = Tween(begin: begin, end: end)
-//               .chain(CurveTween(curve: Curves.easeInOut));
-//           final Animation<double> doubleAnimation = animation.drive(tween);
-//           return FadeTransition(
-//             opacity: doubleAnimation,
-//             child: child,
-//           );
-//         },
-//       ),
-//     );
-//   }
+//   bool isSetUpFinish = false;
+//   bool isLogoVisibility = true;
+//   bool isAnimationFinish = false;
+//   //   bool isSetUpFinish = false;
+//   //   bool isAnimationFinish = false;
+//   //   bool isLogoVisibility = true;
+//   //   bool isComeBackLogo = false;
 //
 //   void measureDisplaySize(Size size) async {
 //     displaySize = size;
 //   }
+//
+//   Future<void> setUp() async {
+//     logoPosition.x = (displaySize.width - logoSize.width) / 2;
+//     logoPosition.y = (displaySize.height - logoSize.height) / 2;
+//     isSetUpFinish = true;
+//     await Future.delayed(const Duration(seconds: 1));
+//     notifyListeners();
+//   }
+//
+//   Future<void> changeLogoPosition(DraggableDetails details) async {
+//     logoPosition.x = details.offset.dx;
+//     logoPosition.y = details.offset.dy;
+//     logoVisibility();
+//     notifyListeners();
+//   }
+//
+//   void logoVisibility() {
+//     if(logoPosition.x > displaySize.width - (logoSize.width / 2) || logoPosition.x < -(logoSize.width / 2)){
+//       isLogoVisibility = false;
+//     }
+//     if(logoPosition.y < -(logoSize.height / 2) || logoPosition.y > displaySize.height - (logoSize.height /2)){
+//       isLogoVisibility = false;
+//     }
+//   }
+//
+//   Future<void> comeBackLogo() async {
+//     setUp();
+//     await Future.delayed(Duration(seconds: Utils.comeBackSecond));
+//     isLogoVisibility = true;
+//     notifyListeners();
+//   }
+//
+//   void transitionScreen(BuildContext context) async {
+//     // await Future.delayed(Duration(seconds: Utils.transitionSecond));
+//     Navigator.of(context).push(
+//       PageRouteBuilder(
+//           pageBuilder: (context, animation, secondaryAnimation){
+//             return FakePage();
+//           },
+//         transitionsBuilder: (context, animation, secondaryAnimation, child){
+//             const double begin = 0.0;
+//             const double end = 1.0;
+//             final Animatable<double> tween = Tween(begin: begin, end: end).chain(CurveTween(curve: Curves.easeInOut));
+//             final Animation<double> doubleAnimation = animation.drive(tween);
+//             return FadeTransition(
+//               opacity: doubleAnimation,
+//               child: child,
+//             );
+//         }
+//       )
+//     );
+//   }
+//
+//     // Navigator.of(context).push(
+//     //   PageRouteBuilder(
+//     //     pageBuilder: (context, animation, secondaryAnimation) {
+//     //       return FakePage();
+//     //     },
+//     //     transitionsBuilder: (context, animation, secondaryAnimation, child) {
+//     //       const double begin = 0.0;
+//     //       const double end = 1.0;
+//     //       final Animatable<double> tween = Tween(begin: begin, end: end)
+//     //           .chain(CurveTween(curve: Curves.easeInOut));
+//     //       final Animation<double> doubleAnimation = animation.drive(tween);
+//     //       return FadeTransition(
+//     //         opacity: doubleAnimation,
+//     //         child: child,
+//     //       );
+//     //     },
+//     //   ),
+//     // );
+//
 // }
+
+class SplashModel extends ChangeNotifier {
+  Position logoPosition = Position(x: 0, y: 0);
+
+  //isSetUpはアプリ起動の再現が行われたかの判断
+  bool isSetUpFinish = false;
+  bool isAnimationFinish = false;
+  bool isLogoVisibility = true;
+  bool isComeBackLogo = false;  //true=ロゴが戻ってきた
+
+  Size displaySize = const Size(0, 0);
+  Size logoSize = const Size(200, 200);
+
+  Future<void> setUp() async {
+    logoPosition.x = (displaySize.width - logoSize.width) / 2;
+    logoPosition.y = (displaySize.height - logoSize.height) / 2;
+
+    //画面が立ち上がってから、アニメーションが発生するまでの長さ
+    await Future.delayed(const Duration(milliseconds: 100));
+    isSetUpFinish = true;
+
+    notifyListeners();
+
+    //スプラッシュ画面が表示される秒数
+    await Future.delayed(const Duration(seconds: 1));
+  }
+
+  Future<void> changeLogoPosition(
+      DraggableDetails details, BuildContext context) async {
+    logoPosition.x = details.offset.dx;
+    logoPosition.y = details.offset.dy;
+    await logoVisibility(context);
+    notifyListeners();
+  }
+
+  Future<void> logoVisibility(BuildContext context) async {
+    if (logoPosition.x < -(logoSize.width / 3) ||
+        logoPosition.x + logoSize.width - displaySize.width >
+            logoSize.width / 3) {
+      isLogoVisibility = false;
+    }
+    if (logoPosition.y < -(logoSize.height / 3) ||
+        logoPosition.y + logoSize.height - displaySize.height >
+            logoSize.height / 3) {
+      isLogoVisibility = false;
+    }
+  }
+
+  Future<void> comeBackLogo(BuildContext context) async {
+    setUp();
+    await Future.delayed(
+        Duration(seconds: Utils.comeBackSecond - Utils.notifySecond))
+        .then((value) {
+      HapticFeedback.heavyImpact();
+    });
+    await Future.delayed(Duration(seconds: Utils.notifySecond));
+    isComeBackLogo = true;
+    notifyListeners();
+  }
+
+  void transitionScreen(BuildContext context) {
+    Navigator.of(context).push(
+      PageRouteBuilder(
+        pageBuilder: (context, animation, secondaryAnimation) {
+          return FakePage();
+        },
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          const double begin = 0.0;
+          const double end = 1.0;
+          final Animatable<double> tween = Tween(begin: begin, end: end)
+              .chain(CurveTween(curve: Curves.easeInOut));
+          final Animation<double> doubleAnimation = animation.drive(tween);
+          return FadeTransition(
+            opacity: doubleAnimation,
+            child: child,
+          );
+        },
+      ),
+    );
+  }
+
+  void measureDisplaySize(Size size) async {
+    displaySize = size;
+  }
+}
--- lib/splash/splash_page.dart
+++ lib/splash/splash_page.dart
@@ -10,21 +10,15 @@ class SplashPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-  final Size displaySize = MediaQuery.of(context).size;
-
+    final Size displaySize = MediaQuery.of(context).size;
     return Scaffold(
       body: Consumer<SplashModel>(
-        builder: (
-        BuildContext context,
-            model,
-            Widget? child,
-        ){
-
+        builder: (BuildContext context, model, Widget? child,){
           model.measureDisplaySize(displaySize);
+          Duration animationMilliseconds = const Duration(milliseconds: 300);
 
-          if(model.isSetUpFinish) {
+          if(!model.isSetUpFinish) {
             Future.delayed(const Duration(seconds: 1));
-
             model.setUp().then((value){
               if (Utils.setUpNumber < Utils.frequencyNumber) {
                 Utils.setUpNumber++;
@@ -35,27 +29,62 @@ class SplashPage extends StatelessWidget {
             });
           }
 
-
           return Stack(
             children: [
               SizedBox(
-                width: double.infinity,
-                height: double.infinity,
-                child: Image.asset('images/line-background.png'),
+                child: Image.asset("images/performance-page.jpg"),
               ),
-              if(model.isLogoVisibility)
+              AnimatedContainer(
+                onEnd: (){
+                  model.isAnimationFinish = true;
+                  model.notifyListeners();
+                },
+                curve: Curves.easeInOutCirc,
+                duration: const Duration(milliseconds: 300),
+                padding: EdgeInsets.only(
+                  top: !model.isSetUpFinish ? Utils.splashPosition.y : 0,
+                  left: !model.isSetUpFinish ? Utils.splashPosition.x : 0,
+                  right: !model.isSetUpFinish ? model.displaySize.width - Utils.splashPosition.x - 80 : 0,
+                  bottom: !model.isSetUpFinish ? model.displaySize.height - Utils.splashPosition.y - 80 : 0,
+                ),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    image: const DecorationImage(
+                      image: AssetImage("images/line-background.png"),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                  child: Center(
+                    child: SizedBox(
+                      height: model.logoSize.height,
+                      width: model.logoSize.width,
+                      child: Image.asset("images/line-logo.png"),
+                    ),
+                  )
+                ),
+              ),
+              if (model.isAnimationFinish && !model.isComeBackLogo)
+                SizedBox(
+                  child: Image.asset("images/line-background.png"),
+                ),
+              if (model.isAnimationFinish &&
+                  model.isLogoVisibility &&
+                  !model.isComeBackLogo)
+
               Positioned(
                 left: model.logoPosition.x,
                 top: model.logoPosition.y,
                 child: Draggable(
                   onDragEnd: (details) async {
-                    await model.changeLogoPosition(details);
-                      if (model.isLogoVisibility == false) {
-                         await model.comeBackLogo();
-                         await Future.delayed(Duration(seconds: Utils.transitionSecond));
-                         // ignore: use_build_context_synchronously
-                         model.transitionScreen(context);
-                      }
+                    await model.changeLogoPosition(details, context);
+                    if (model.isLogoVisibility == false) {
+                      // ignore: use_build_context_synchronously
+                      await model.comeBackLogo(context);
+                      await Future.delayed(Duration(seconds: Utils.transitionSecond));
+                      // ignore: use_build_context_synchronously
+                      model.transitionScreen(context);
+                    }
                   },
                   childWhenDragging: Container(), //ドラッグしている時のドラッグ前のwidgetの見え方
                   feedback: SizedBox( //ドラッグしている時の見え方
@@ -77,176 +106,3 @@ class SplashPage extends StatelessWidget {
     );
   }
 }
-
-
-// class SplashPage extends StatelessWidget {
-//   const SplashPage({Key? key}) : super(key: key);
-//   @override
-//   Widget build(BuildContext context) {
-//     Size display = MediaQuery.of(context).size;
-//     Duration animationMilliseconds = const Duration(milliseconds: 300);
-//
-//     return Scaffold(
-//       body: Consumer<SplashModel>(
-//         builder: (BuildContext context, model, Widget? child) {
-//           model.measureDisplaySize(display);
-//           if (!model.isSetUpFinish) {
-//             model.setUp().then(
-//                   (value) {
-//                 if (Utils.setUpNumber < Utils.frequencyNumber) {
-//                   Utils.setUpNumber++;
-//                   model.transitionScreen(context);
-//                 } else {
-//                   Utils.setUpNumber = 0;
-//                 }
-//               },
-//             );
-//           }
-//           return Stack(
-//             children: [
-//               buildBackgroundImage(),
-//               buildAnimatedContainer(model, animationMilliseconds),
-//               //以下logoを移動させるWidget
-//               if (model.isAnimationFinish && !model.isComeBackLogo)
-//                 buildSplashBackImage(),
-//               if (model.isAnimationFinish &&
-//                   model.isLogoVisibility &&
-//                   !model.isComeBackLogo)
-//                 Positioned(
-//                   left: model.logoPosition.x,
-//                   top: model.logoPosition.y,
-//                   child: Draggable(
-//                     onDragEnd: (details) async {
-//                       await model.changeLogoPosition(details, context);
-//                       if (model.isLogoVisibility == false) {
-//                         await model.comeBackLogo(context);
-//                         await Future.delayed(Duration(
-//                             seconds: Utils.transitionSecond -
-//                                 Utils.notifySecond))
-//                             .then((value) {
-//                           HapticFeedback.heavyImpact();
-//                         });
-//                         await Future.delayed(
-//                             Duration(seconds: Utils.notifySecond));
-//                         model.transitionScreen(context);
-//                       }
-//                     },
-//                     childWhenDragging: Container(),
-//                     feedback: Container(
-//                       height: model.logoSize.height,
-//                       width: model.logoSize.width,
-//                       child: ClipRRect(
-//                         borderRadius: BorderRadius.circular(15),
-//                         child: Utils.logoImageFile != null
-//                             ? Image.file(
-//                           Utils.logoImageFile!,
-//                           fit: BoxFit.fill,
-//                         )
-//                             : Image.asset(
-//                           'images/line-logo.png',
-//                           fit: BoxFit.fill,
-//                         ),
-//                       ),
-//                     ),
-//                     child: Container(
-//                       height: model.logoSize.height,
-//                       width: model.logoSize.width,
-//                       child: ClipRRect(
-//                         borderRadius: BorderRadius.circular(15),
-//                         child: Utils.logoImageFile != null
-//                             ? Image.file(
-//                           Utils.logoImageFile!,
-//                           fit: BoxFit.fill,
-//                         )
-//                             : Image.asset(
-//                           'images/line-logo.png',
-//                           fit: BoxFit.fill,
-//                         ),
-//                       ),
-//                     ),
-//                   ),
-//                 ),
-//             ],
-//           );
-//         },
-//       ),
-//     );
-//   }
-//
-//
-//   Container buildSplashBackImage(){
-//     return Container(
-//       width: double.infinity,
-//       height: double.infinity,
-//       child: Utils.fakeColorFile != null
-//           ? Image.file(Utils.fakeColorFile!, fit: BoxFit.fill,)
-//           : Image.asset("images/line-background.png",fit: BoxFit.fill,),
-//     );
-//   }
-//
-//   AnimatedContainer buildAnimatedContainer(SplashModel model, Duration animationMilliseconds){
-//     return AnimatedContainer(
-//       onEnd: (){
-//         model.isAnimationFinish = true;
-//         model.notifyListeners();
-//       },
-//       padding: EdgeInsets.only(
-//         top: !model.isSetUpFinish ? Utils.splashPosition.y : 0,
-//         left: !model.isSetUpFinish ? Utils.splashPosition.x : 0,
-//         right: !model.isSetUpFinish ? model.displaySize.width - Utils.splashPosition.x - 80 : 0,
-//         bottom: !model.isSetUpFinish ? model.displaySize.height - Utils.splashPosition.y - 80 : 0,
-//       ),
-//       curve: Curves.easeInOutCirc,
-//       height: model.displaySize.height,
-//       width: model.displaySize.width,
-//       duration: animationMilliseconds,
-//       child: AnimatedContainer(
-//         decoration: BoxDecoration(
-//           borderRadius: BorderRadius.circular(15),
-//           image: Utils.fakeColorFile != null ? DecorationImage(
-//               image: FileImage(
-//                 Utils.fakeColorFile!,
-//               ),
-//             fit: BoxFit.fill
-//           ) : const DecorationImage(
-//               image: AssetImage('images/line-background.png'),
-//               fit: BoxFit.fill),
-//         ),
-//         duration: animationMilliseconds,
-//         child: ClipRRect(
-//           borderRadius: BorderRadius.circular(10),
-//           child: Center(
-//             child: AnimatedContainer(
-//               curve: Curves.easeInOutCirc,
-//               duration: animationMilliseconds,
-//               height: model.logoSize.height,
-//               width: model.logoSize.width,
-//               child: Utils.logoImageFile != null
-//                   ? Image.file(
-//                 Utils.logoImageFile!,
-//                 fit: BoxFit.fill,
-//               )
-//                   : Image.asset(
-//                 'images/line-logo.png',
-//                 fit: BoxFit.fill,
-//               ),
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-//
-//   SizedBox buildBackgroundImage() {
-//     return SizedBox(
-//       height: double.infinity,
-//       width: double.infinity,
-//       child: Utils.backgroundImageFile == null
-//           ? Image.asset('images/performance-page.jpg')
-//           : Image.file(
-//         Utils.backgroundImageFile!,
-//         fit: BoxFit.fill,
-//       ),
-//     );
-//   }
-// }
