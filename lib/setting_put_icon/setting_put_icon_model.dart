e7d8aee24008b35c0ca81fa7cee43330b1f92cdd
3balljuggler <3balljuggling@gmail.com>
2023-01-09 13:15:50 +0900

    アイコンのセーフエリアの設定

--- lib/setting_put_icon/setting_put_icon_model.dart
+++ lib/setting_put_icon/setting_put_icon_model.dart
@@ -3,10 +3,21 @@ import 'package:fake_icon_practice/position_model.dart';
 
 class SettingPutIconModel extends ChangeNotifier {
   Size? displaySize;
+  late Size displayMaxSafeArea = Size(displaySize!.width - 5, displaySize!.height - 10);
+  late Size displayMinSafeArea = const Size(5, 40);
 
   void dragEnd(dragDetails, Position position) {
-    position.x = dragDetails.offset.dx;
-    position.y = dragDetails.offset.dy;
+    if(displayMaxSafeArea.width < dragDetails.offset.dx + 65
+        || displayMaxSafeArea.height < dragDetails.offset.dy + 65
+        || displayMinSafeArea.width > dragDetails.offset.dx
+        || displayMinSafeArea.height > dragDetails.offset.dy
+    ){
+      position.x = displaySize!.width * 0.5 - 30;
+      position.y = displaySize!.height * 0.5 - 30;
+    } else {
+      position.x = dragDetails.offset.dx;
+      position.y = dragDetails.offset.dy;
+    }
     Position.isChange = true;
     notifyListeners();
   }
